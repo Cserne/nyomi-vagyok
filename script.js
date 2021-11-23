f89795e78dcd43d732a61191ac000cb4091cc63e
Cserne <angeli.cserne@gmail.com>
2021-11-23 18:13:12 +0100

    Changes added.

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+function loadEvent() {
+
+    let body = document.body;
+    body.classList.add("body");
+        
+    let root = document.getElementById("root");
+    
+    let headerSignature = document.createElement("div");
+    headerSignature.classList.add("header", "headerSignature")
+    headerSignature.textContent = "Thibaut Foussard";
+    root.appendChild(headerSignature);
+    
+    let headerAbout = document.createElement("div");
+    headerAbout.classList.add("header", "headerAbout")
+    headerAbout.textContent = "About";
+    
+    let headerContact = document.createElement("div");
+    headerContact.classList.add("header", "headerContact")
+    headerContact.textContent = "Contact";
+    
+    let headerMenu = document.createElement("div");
+    headerMenu.classList.add("header", "headerMenu")
+    headerMenu.appendChild(headerAbout);
+    headerMenu.appendChild(headerContact);
+    root.appendChild(headerMenu);
+    
+    let header = document.createElement("header");
+    header.classList.add("header");
+    header.appendChild(headerSignature);
+    header.appendChild(headerMenu);
+    root.appendChild(header);
+    }
+    
+    window.addEventListener("load", loadEvent)
+    */
+
